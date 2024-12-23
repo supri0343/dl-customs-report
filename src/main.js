9b3356dca59c17683d3038bc9d703dcc912d1ecf
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-12-23 10:08:12 +0700

    2024/27/Fix/EndPoint-Inventory

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ export async function configure(aurelia) {
       var auth = "https://com-danliris-service-auth-v8.azurewebsites.net/v1/";
       var customsReport = "https://com-danliris-service-support.azurewebsites.net/v1/" ;
       var purchasing = "https://com-danliris-service-purchasing.azurewebsites.net/v1/" ;
-      var inventory = "https://com-danliris-service-inventory-prd.azurewebsites.net/v1/";
+      var inventory = "https://com-danliris-service-inventory-prd-v8.azurewebsites.net/v1/";
       var garmentProduction = "https://com-danliris-service-garment.azurewebsites.net/";
       var itInventory = "https://com-danliris-service-it-inventory.azurewebsites.net/v1/";
 
